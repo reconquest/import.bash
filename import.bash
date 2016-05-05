1fd07a583c107edb6256e811280e146ed42065eb
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-05-05 13:25:34 +0600

    remove debug leftovers

--- import.bash
+++ import.bash
@@ -5,9 +5,6 @@ function import() {
 
     local name=$1
 
-    printf 'XXXXXX import.bash:7: $0: %q\n' $0 >&2
-    printf 'XXXXXX import.bash:7: $name: %q\n' $name >&2
-    printf 'XXXXXX import.bash:7: ${BASH_SOURCE[@]}: %q\n' ${BASH_SOURCE[@]} >&2
 
     local base_dir=$(cd "$(dirname "${BASH_SOURCE[1]}")" && pwd)
     source "$base_dir/vendor/$name/${name#*/}.bash"
