7cf0613e6ae2ec9f91bd250d6501a7bfe9a1c4d9
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-05-06 12:55:59 +0600

    import.bash: import from git root

--- import.bash
+++ import.bash
@@ -1,9 +1,18 @@
 function import() {
     if [ $# -lt 1 ]; then
         echo first argument should be name to import
+        return 1
     fi 2>&1
 
     local name=$1
     local base_dir=$(dirname "$(readlink -f "${BASH_SOURCE[1]}")")
+
+    local git_root_dir
+    if git_root_dir=$(
+        cd "$base_dir" && git rev-parse --show-toplevel 2>/dev/null)
+    then
+        base_dir="$git_root_dir"
+    fi
+
     source "$base_dir/vendor/$name.bash/${name##*/}.bash"
 }
---
+++ tests/testcases/can-import-from-git-root-dir.test.sh
@@ -0,0 +1,20 @@
+tests:make-tmp-dir -p vendor/x/lib-a.bash/lib-a
+tests:make-tmp-dir -p blah/
+
+tests:eval git init
+
+tests:put vendor/x/lib-a.bash/lib-a.bash <<EOF
+echo -n 1
+EOF
+
+tests:put blah/script.bash <<EOF
+source import.bash
+
+import x/lib-a
+EOF
+
+tests:eval git add script.bash
+tests:eval git commit -m initial
+
+tests:ensure source blah/script.bash
+tests:assert-stdout 1
